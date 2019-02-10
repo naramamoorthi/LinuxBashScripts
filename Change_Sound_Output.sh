603e11228b3e127cf9c06e606dbc364c81b90bbd
Navin Ramamoorthi <43849268+naramamoorthi@users.noreply.github.com>
2019-02-10 22:25:09 +0800

    Update Change_Sound_Output.sh

--- Change_Sound_Output.sh
+++ Change_Sound_Output.sh
@@ -1,3 +1,5 @@
+#This script was created to address an issue in Ubuntu where the sound will not change to the active output device.
+
 input=$(pactl list sink-inputs | grep 'Input')
 stream=${input//[!0-9]/}
 pacmd move-sink-input $stream 1
